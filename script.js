f0fd115a2fd64d4dc76b4882618cb47efb5a5a41
PRIYANKA CHAUDHARI <117078620+1PRIYANKA1@users.noreply.github.com>
2023-02-14 03:47:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+//shop, cart//
+
+let arrow = document.querySelectorAll(".arrow");
+for (var i = 0; i < arrow.length; i++) {
+  arrow[i].addEventListener("click", (e)=>{
+ let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+ arrowParent.classList.toggle("showMenu");
+  });
+}
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
+console.log(sidebarBtn);
+sidebarBtn.addEventListener("click", ()=>{
+  sidebar.classList.toggle("close");
+});
+
+// products //
+
+var mainimg=document.getElementById("mainimg");
+var smallimg=document.getElementsByClassName("small-img");
+
+smallimg[0].onclick = function(){
+    mainimg.src = smallimg[0].src;
+}
+smallimg[1].onclick = function(){
+    mainimg.src = smallimg[1].src;
+}
+smallimg[2].onclick = function(){
+    mainimg.src = smallimg[2].src;
+}
+smallimg[3].onclick = function(){
+    mainimg.src = smallimg[3].src;
+}
